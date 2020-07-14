6a7eb9b1aefb3b408a4fe609fa4bd50aaf5906d7
begantsovaM <56974757+begantsovaM@users.noreply.github.com>
2020-07-14 16:24:44 +0600

    Add files via upload

--- registration/email_sender.py
+++ registration/email_sender.py
@@ -4,12 +4,27 @@ from string import Template
 from pathlib import Path  # os.path
 
 
-def send_mail(mail, code):
-    html = Template(Path('index.html').read_text())
+def restore(mail, code):
+    html = Template(Path('restore.html').read_text())
     email = EmailMessage()
     email['from'] = 'Registration'
     email['to'] = mail
     email['subject'] = 'Password recovery'
+    email.set_content(html.substitute(
+        {'login': mail.split("@")[0], 'garbage': code}), 'html')
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('reiko.off@gmail.com', 'reikoisthebest_1')
+        smtp.send_message(email)
+
+
+def confirm(mail, code):
+    html = Template(Path('confirmation.html').read_text())
+    email = EmailMessage()
+    email['from'] = 'Registration'
+    email['to'] = mail
+    email['subject'] = 'Email confirmation'
     email.set_content(html.substitute(
         {'login': mail.split("@")[0], 'garbage': code}), 'html')
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
