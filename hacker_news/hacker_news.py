a159b1759a6dd6c22bfb6fc5489aeff3f328f4f7
begantsovaM <56974757+begantsovaM@users.noreply.github.com>
2020-07-17 01:44:28 +0600

    Update and rename hacker_news.py to hacker_news/hacker_news.py

--- hacker_news/hacker_news.py
+++ hacker_news/hacker_news.py
@@ -4,14 +4,6 @@ import pprint
 import sys
 
 
-def print_stories_hn(hnlist):
-    i = 1
-    for items in hnlist:
-        print(
-            f"{i}. {items['title']}\nLink: {items['link']}\nVotes: {items['votes']}\n")
-        i += 1
-
-
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
@@ -37,10 +29,27 @@ def scan_pages(pages, stories):
     return sort_stories_by_votes(stories)
 
 
+def create_site(stories):
+    with open('index.html', 'w') as site:
+        site.write(
+            "<!DOCTYPE html>\n<head>\n<title>Hacker news</title>\n<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">\n<link href=\"https://fonts.googleapis.com/css2?family=Varta&display=swap\" rel=\"stylesheet\"></head>\n<body>\n<h1>Hacker news</h1>\n<table>")
+        site.write("<tr><th>#</th><th>Title</th><th>Votes</th></tr>")
+        i = 1
+        for items in stories:
+            site.write("<tr>")
+            site.write(f"<td>{i}</td>\n")
+            site.write(
+                f"<td><a href=\"{items['link']}\">{items['title']}</a></td>\n")
+            site.write(f"<td>{items['votes']}</td>\n")
+            site.write("</tr>")
+            i += 1
+        site.write("</table>\n</body>")
+
+
 def main(pages):
     stories = []
     stories = scan_pages(pages, stories)
-    print_stories_hn(stories)
+    create_site(stories)
     return("Done.")
 
 
