f97fe4bbb3c67c4618b95c85e91e89de080c4add
begantsovaM <56974757+begantsovaM@users.noreply.github.com>
2020-07-31 15:44:18 +0600

    Create callback_data.py

---
+++ animemash/keyboards/callback_data.py
@@ -0,0 +1,6 @@
+from aiogram.utils.callback_data import CallbackData
+
+anime_choice = CallbackData(
+    "anime", "button", "winner", "anime_one", "anime_one_rate", "anime_two", "anime_two_rate")
+
+gen_anime = CallbackData("generate", "action")
